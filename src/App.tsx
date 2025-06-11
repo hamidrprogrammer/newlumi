8446d254e1e27b449ab628d5abbe99f9214f5b90
hamidrprogrammer <hamidrprogrammer@gmail.com>
2025-06-11 10:36:51 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,33 @@ const correctUsername = "admin";
 const correctPassword = "KyXB6E9P9JGCUp7@QEhzA1ocAK";
 
 const App: React.FC = () => {
-  const [authorized, setAuthorized] = useState<boolean>(false);
+    const [authorized, setAuthorized] = useState<boolean>(false);
 
-  // useEffect(() => {
-  //   const storedAuth = localStorage.getItem('authorized');
+  useEffect(() => {
+    const storedAuth = localStorage.getItem('authorized');
 
-  //   if (storedAuth === 'true') {
-  //     setAuthorized(true);
-  //     return;
-  //   }
+    if (storedAuth === 'true') {
+      setAuthorized(true);
+      return;
+    }
 
-  //   const askCredentials = () => {
-  //     const username = prompt("Enter username:");
-  //     const password = prompt("Enter password:");
+    const askCredentials = () => {
+      const username = prompt("Enter username:");
+      const password = prompt("Enter password:");
 
-  //     if (username === correctUsername && password === correctPassword) {
-  //       localStorage.setItem('authorized', 'true');
-  //       setAuthorized(true);
-  //     } else {
-  //       alert("Username or password is incorrect!");
-  //       askCredentials();
-  //     }
-  //   };
+      if (username === correctUsername && password === correctPassword) {
+        localStorage.setItem('authorized', 'true');
+        setAuthorized(true);
+      } else {
+        alert("Username or password is incorrect!");
+        askCredentials();
+      }
+    };
 
-  //   askCredentials();
-  // }, []);
+    askCredentials();
+  }, []);
+
+  if (!authorized) return null;
 
 
 
